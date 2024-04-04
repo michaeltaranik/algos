7be9cdd04a76c4ec7a13089de295a15a81e4be1a
michaeltaranik <taranikmichael32@gmail.com>
2024-04-04 08:57:31 +0200

    heap

--- sort_recap.py
+++ sort_recap.py
@@ -1,5 +1,5 @@
 def heapify(arr, parent, end):
-    largest = parent 
+    largest = parent
     left = parent * 2 + 1
     right = parent * 2 + 2
 
@@ -9,23 +9,23 @@ def heapify(arr, parent, end):
     if right < end and arr[right] > arr[largest]:
         largest = right
 
-    if largest != parent:
+    if parent != largest:
         arr[parent], arr[largest] = arr[largest], arr[parent]
-        heapify(arr, largest, end)
+        heapify(arr, largest, end) 
 
 def heap_sort(arr):
     end = len(arr)
 
-    for i in range(end // 2 - 1, -1, -1):
-        heapify(arr, i, end-1)
+    for i in range(end // 2, -1, -1):
+        heapify(arr, i, end)
 
     for j in range(end - 1, -1, -1):
-        arr[0], arr[j] = arr[j], arr[0]
-        heapify(arr, 0, j)
+        arr[0], arr[j] =  arr[j], arr[0] 
+        heapify(arr, j - 1, end)
+
     return arr
 
-
-# print(heap_sort([90,10,30,20,50,70,40,30,40,100,50]))
+print(heap_sort([90,10,30,20,50,70,40,30,40,100,50]))
 
 def partition(arr, low, high):
     j = low - 1
@@ -76,33 +76,33 @@ def max_heap_insert(arr, x):
 
 H = [3, 7, 3, 2, 9, 5, 9, 8, 5, 2, 9, 4, 7, 3, 9]
 
-arr = (max_heap_insert([], 3))
-print(arr)
-arr = (max_heap_insert(arr, 7))
-print(arr)
-arr = (max_heap_insert(arr, 3))
-print(arr)
-arr = (max_heap_insert(arr, 2))
-print(arr)
-arr = (max_heap_insert(arr, 9))
-print(arr)
-arr = (max_heap_insert(arr, 5))
-print(arr)
-arr = (max_heap_insert(arr, 9))
-print(arr)
-arr = (max_heap_insert(arr, 8))
-print(arr)
-arr = (max_heap_insert(arr, 5))
-print(arr)
-arr = (max_heap_insert(arr, 2))
-print(arr)
-arr = (max_heap_insert(arr, 9))
-print(arr)
-arr = (max_heap_insert(arr, 4))
-print(arr)
-arr = (max_heap_insert(arr, 7))
-print(arr)
-arr = (max_heap_insert(arr, 3))
-print(arr)
-arr = (max_heap_insert(arr, 9))
-print(arr)
+# arr = (max_heap_insert([], 3))
+# print(arr)
+# arr = (max_heap_insert(arr, 7))
+# print(arr)
+# arr = (max_heap_insert(arr, 3))
+# print(arr)
+# arr = (max_heap_insert(arr, 2))
+# print(arr)
+# arr = (max_heap_insert(arr, 9))
+# print(arr)
+# arr = (max_heap_insert(arr, 5))
+# print(arr)
+# arr = (max_heap_insert(arr, 9))
+# print(arr)
+# arr = (max_heap_insert(arr, 8))
+# print(arr)
+# arr = (max_heap_insert(arr, 5))
+# print(arr)
+# arr = (max_heap_insert(arr, 2))
+# print(arr)
+# arr = (max_heap_insert(arr, 9))
+# print(arr)
+# arr = (max_heap_insert(arr, 4))
+# print(arr)
+# arr = (max_heap_insert(arr, 7))
+# print(arr)
+# arr = (max_heap_insert(arr, 3))
+# print(arr)
+# arr = (max_heap_insert(arr, 9))
+# print(arr)
